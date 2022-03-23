53f7b1914a8ac8a33a8194aed138cf6036b09498
Gurpal Singh <gillgurpal699@gmail.com>
2022-03-23 14:53:03 +0530

    refactor component code

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,12 @@ fn main() {
     fs::create_dir(&args.name).expect("Failed to Create Folder");
     let component_file_name: String;
     let stylesheet_file_name: String;
-    let component_file_component: String;
-    let stylesheet_file_content = format!(".h1{{\n\ttext-align:center;\n}}");
+    let component_file_content: String;
+    let stylesheet_file_content = format!(
+        ".h1{{
+    text-align:center;
+}}"
+    );
     if args.typescript == true {
         component_file_name = "index.tsx".to_string();
     } else {
@@ -42,20 +46,30 @@ fn main() {
     }
 
     if stylesheet_file_name.contains("module") {
-        component_file_component = format!(
-            "import styles from \"./{}\";\nexport default function {}(){{\n\n\treturn(\n\t\t<h1 className={{styles.h1}} >{} works</h1>\n\t)\n}}",stylesheet_file_name,
-            args.name, args.name
-        );
+        component_file_content = format!(
+            "import styles from \"./{}\";
+export default function {}(){{
+    return <h1 className=\"{{styles.h1}}\"> {} Works</h1>
+    
+}}
+",
+            stylesheet_file_name, args.name, args.name
+        )
     } else {
-        component_file_component =format!(
-            "import \"./{}\";\nexport default function {}(){{\n\n\treturn(\n\t\t<h1 className=\"h1\" >{} works</h1>\n\t)\n}}",stylesheet_file_name,
-            args.name, args.name
-        );
+        component_file_content = format!(
+            "import \"./{}\";
+export default function {}(){{
+    return <h1 className=\"h1\">{} Works</h1>
+}}
+        ",
+            stylesheet_file_name, args.name, args.name
+        )
+        .to_string();
     }
 
     fs::write(
         format!("{}/{}", args.name, component_file_name),
-        component_file_component,
+        component_file_content,
     )
     .expect("Failed to Create Component File");
     fs::write(
