48d1c9a9dfc1e1508fa42820e8ca2a837e8feb98
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-03-29 15:20:23 +0300

    Added cross compilation targets for musl

--- actions/linux-musl/entrypoint.sh
+++ actions/linux-musl/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+mkdir .cargo
 
-cargo build --release -vv
-cd target/release/ && tar -cvf react-x86_64-unknown-linux-musl.tar.gz react && cd ../..
+echo '[target.i686-unknown-linux-musl]' >>.cargo/config
+echo 'linker = "i686-linux-musl-gcc"' >>.cargo/config
+
+echo '[target.aarch64-unknown-linux-musl]' >>.cargo/config
+echo 'linker = "aarch64-linux-musl-gcc"' >>.cargo/config
+
+cargo build -Zmultitarget --target=x86_64-unknown-linux-musl --target=i686-unknown-linux-musl --target=aarch64-unknown-linux-musl --release -vv
+
+cd target/x86_64-unknown-linux-musl/release/ && tar -cvf react-x86_64-unknown-linux-musl.tar.gz react && cd ../../..
+cd target/i686-unknown-linux-musl/release/ && tar -cvf react-i686-unknown-linux-musl.tar.gz react && cd ../../..
+cd target/aarch64-unknown-linux-musl/release && tar -cvf react-aarch64-unknown-linux-musl.tar.gz react && cd ../../..
